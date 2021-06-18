a2fda80ad594b78a416f2067b51841ae772ae234
NaraWee <wnl1508@naver.com>
2021-06-19 02:32:48 +0900

    chap06-config-lecture-source

---
+++ chap06-config-lecture-source/src/com/greedy/section01/config/Application.java
@@ -0,0 +1,10 @@
+package com.greedy.section01.config;
+
+import com.greedy.common.Template;
+
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("getSqlSession : " + Template.getSqlSession());
+	}
+}
